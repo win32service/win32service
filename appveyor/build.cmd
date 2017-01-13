44d6db6deae6cb774959179bb8de4e4616e635cc
Fernando Herrero <ininflamable@gmail.com>
2017-01-13 16:43:07 +0100

    Used php-sdk-binary-tools.git
    Used php-sdk-binary-tools.git instead of
    php-sdk-binary-tools-20110915.zip

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -1,28 +1,45 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
+	if not exist "%SDK_CACHE%" (
+		echo Cloning remote SDK repository
+		git clone --branch %SDK_BRANCH% %SDK_REMOTE% "%SDK_CACHE%" 2>&1
+	) else (
+		echo Fetching remote SDK repository
+		git --git-dir="%SDK_CACHE%\.git" --work-tree="%SDK_CACHE%" fetch --prune origin 2>&1
+		echo Checkout SDK repository branch
+		git --git-dir="%SDK_CACHE%\.git" --work-tree="%SDK_CACHE%" checkout --force %SDK_BRANCH%
+	)
+
 	echo git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
-	git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
+	git clone --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
 
 	xcopy %APPVEYOR_BUILD_FOLDER% C:\projects\php-src\ext\win32service\ /s /e /y /f
 
 	xcopy %APPVEYOR_BUILD_FOLDER%\LICENSE %APPVEYOR_BUILD_FOLDER%\artifacts\ /y /f
 	xcopy %APPVEYOR_BUILD_FOLDER%\examples %APPVEYOR_BUILD_FOLDER%\artifacts\examples\ /y /f
 
-	cd %APPVEYOR_BUILD_FOLDER%\appveyor
-	wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/php-sdk-binary-tools-20110915.zip
-	7z x -y php-sdk-binary-tools-20110915.zip -oC:\projects\php-sdk
+	rem cd %APPVEYOR_BUILD_FOLDER%\appveyor
+	rem wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/php-sdk-binary-tools-20110915.zip
+	rem 7z x -y php-sdk-binary-tools-20110915.zip -oC:\projects\php-sdk
 
 	for %%a in (%ARCHITECTURES%) do (
 		set ARCH=%%a
-		if "!ARCH!"=="amd64" set DEPTS_ARCH=x64
-		if "!ARCH!"=="x86" set DEPTS_ARCH=x86
+		rem if "!ARCH!"=="amd64" set DEPTS_ARCH=x64
+		rem if "!ARCH!"=="x86" set DEPTS_ARCH=x86
 
-		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" !ARCH!
-		call C:\projects\php-sdk\bin\phpsdk_setvars.bat
+		rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" !ARCH!
+		rem call C:\projects\php-sdk\bin\phpsdk_setvars.bat
+		set SDK_RUNNER=%SDK_CACHE%\phpsdk-vc14-!ARCH!.bat
+		if not exist "!SDK_RUNNER!" (
+			echo "!SDK_RUNNER!" doesn't exist
+			exit /b 3
+		)
+
+		call !SDK_RUNNER!
 
 		cd %APPVEYOR_BUILD_FOLDER%\appveyor
-		wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z
-		7z x -y deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z -oC:\projects\php-src
+		wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-!ARCH!.7z -P %CACHE_ROOT%
+		7z x -y deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z -o%CACHE_ROOT%
 
 		for %%z in (%ZTS_STATES%) do (
 			set ZTS_STATE=%%z
@@ -32,7 +49,12 @@ setlocal enableextensions enabledelayedexpansion
 			cd C:\projects\php-src
 
 			call buildconf.bat
-			call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=deps
+
+			if %errorlevel% neq 0 exit /b 3
+
+			call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=%CACHE_ROOT%\deps
+
+			if %errorlevel% neq 0 exit /b 3
 
 			nmake
 
