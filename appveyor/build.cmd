dd7c1359d4646aa476dca79def12176c3c112e40
Fernando Herrero <ininflamable@gmail.com>
2017-01-13 16:55:30 +0100

    Add build_task.cmd

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -2,16 +2,17 @@
 setlocal enableextensions enabledelayedexpansion
 	if not exist "%SDK_CACHE%" (
 		echo Cloning remote SDK repository
-		git clone --branch %SDK_BRANCH% %SDK_REMOTE% "%SDK_CACHE%" 2>&1
+		echo git clone -q --branch %SDK_BRANCH% %SDK_REMOTE% "%SDK_CACHE%"
+		git clone -q --branch %SDK_BRANCH% %SDK_REMOTE% "%SDK_CACHE%"
 	) else (
 		echo Fetching remote SDK repository
-		git --git-dir="%SDK_CACHE%\.git" --work-tree="%SDK_CACHE%" fetch --prune origin 2>&1
+		git --git-dir="%SDK_CACHE%\.git" --work-tree="%SDK_CACHE%" fetch --prune origin
 		echo Checkout SDK repository branch
 		git --git-dir="%SDK_CACHE%\.git" --work-tree="%SDK_CACHE%" checkout --force %SDK_BRANCH%
 	)
 
 	echo git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
-	git clone --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
+	git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
 
 	xcopy %APPVEYOR_BUILD_FOLDER% C:\projects\php-src\ext\win32service\ /s /e /y /f
 
@@ -35,41 +36,8 @@ setlocal enableextensions enabledelayedexpansion
 			exit /b 3
 		)
 
-		call !SDK_RUNNER!
+		call !SDK_RUNNER! -t %APPVEYOR_BUILD_FOLDER%\appveyor\build_task.cmd
 
-		cd %APPVEYOR_BUILD_FOLDER%\appveyor
-		wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-!ARCH!.7z -P %CACHE_ROOT%
-		7z x -y deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z -o%CACHE_ROOT%
-
-		for %%z in (%ZTS_STATES%) do (
-			set ZTS_STATE=%%z
-			if "!ZTS_STATE!"=="enable" set ZTS_SHORT=ts
-			if "!ZTS_STATE!"=="disable" set ZTS_SHORT=nts
-
-			cd C:\projects\php-src
-
-			call buildconf.bat
-
-			if %errorlevel% neq 0 exit /b 3
-
-			call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=%CACHE_ROOT%\deps
-
-			if %errorlevel% neq 0 exit /b 3
-
-			nmake
-
-			if %errorlevel% neq 0 exit /b 3
-
-			nmake install
-
-			if %errorlevel% neq 0 exit /b 3
-
-			cd %APPVEYOR_BUILD_FOLDER%
-
-			if not exist build\ext\php_win32service.dll exit /b 3
-
-			move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-!DEPTS_ARCH!.dll
-		)
 	)
 
 	if "%APPVEYOR_REPO_TAG_NAME%"=="" (
---
+++ appveyor/build_task.cmd
@@ -0,0 +1,36 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+	cd %APPVEYOR_BUILD_FOLDER%\appveyor
+	wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-!ARCH!.7z -P %CACHE_ROOT%
+	7z x -y deps-%PHP_REL%-vc14-%ARCH%.7z -o%CACHE_ROOT%
+
+	for %%z in (%ZTS_STATES%) do (
+		set ZTS_STATE=%%z
+		if "!ZTS_STATE!"=="enable" set ZTS_SHORT=ts
+		if "!ZTS_STATE!"=="disable" set ZTS_SHORT=nts
+
+		cd C:\projects\php-src
+
+		call buildconf.bat
+
+		if %errorlevel% neq 0 exit /b 3
+
+		call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=%CACHE_ROOT%\deps
+
+		if %errorlevel% neq 0 exit /b 3
+
+		nmake
+
+		if %errorlevel% neq 0 exit /b 3
+
+		nmake install
+
+		if %errorlevel% neq 0 exit /b 3
+
+		cd %APPVEYOR_BUILD_FOLDER%
+
+		if not exist build\ext\php_win32service.dll exit /b 3
+
+		move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-%ARCH%.dll
+	)
+endlocal
