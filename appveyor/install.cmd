546467afe122b94ed150a565b65375e063988b15
macintoshplus <jean-baptiste@nahan.fr>
2017-06-13 14:07:09 +0200

    fix git clone

--- appveyor/install.cmd
+++ appveyor/install.cmd
@@ -18,7 +18,11 @@ setlocal enableextensions enabledelayedexpansion
 	)
 
 	echo git clone -q --depth=1 --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
-	git clone -q --depth=1 --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
+	if "%PHP_REL%"=="master" (
+		git clone -q --depth=1 --branch=%PHP_REL% https://github.com/php/php-src C:\projects\php-src
+	) else (
+		git clone -q --depth=1 --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
+	)
 
 	xcopy %APPVEYOR_BUILD_FOLDER% C:\projects\php-src\ext\win32service\ /s /e /y /f
 
