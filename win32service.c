0509809619a7260502d429759459f42bdaa87b8f
Jean-Baptiste NAHAN <jean-baptiste.nahan@inextenso.fr>
2018-08-27 09:59:14 +0200

    #33

--- win32service.c
+++ win32service.c
@@ -650,12 +650,15 @@ static int check_php_version()
 	zval function_name;
 	char *result2 = NULL;
 
-	ZVAL_STRINGL(&function_name, "phpversion", 10);
+	ZVAL_STRING(&function_name, "phpversion");
 
 	if (call_user_function(EG(function_table), NULL, &function_name, &result, 0, NULL TSRMLS_CC) == SUCCESS) {
 		result2 = Z_STRVAL_P(&result);
 		php_printf("VERSION = %s", result2);
 		efree(result2);
+		zval_ptr_dtor(&function_name);
+	} else {
+		php_printf("call_user_function fail");
 	}
 
 	php_printf("M=%d m=%d r=%d\n", PHP_MAJOR_VERSION, PHP_MINOR_VERSION, PHP_RELEASE_VERSION);
