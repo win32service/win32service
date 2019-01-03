537e46e5277361624cb9c6586121ac77bbd32786
macintoshplus <jean-baptiste@nahan.fr>
2019-01-03 15:52:54 +0100

    add load module for test

--- appveyor/test_task.bat
+++ appveyor/test_task.bat
@@ -33,7 +33,7 @@ mkdir c:\tests_tmp
 set TEST_PHP_JUNIT=c:\junit.out.xml
 
 rem cd "%APPVEYOR_BUILD_FOLDER%"
-nmake test TESTS="-q --offline --show-diff --show-slow 1000 --set-timeout 120 -g FAIL,XFAIL,BORK,WARN,LEAK,SKIP --temp-source c:\tests_tmp --temp-target c:\tests_tmp"
+nmake test TESTS="-m %APPVEYOR_BUILD_FOLDER%\build\ext\php_win32service.dll -q --offline --show-diff --show-slow 1000 --set-timeout 120 -g FAIL,XFAIL,BORK,WARN,LEAK,SKIP --temp-source c:\tests_tmp --temp-target c:\tests_tmp"
 
 set EXIT_CODE=%errorlevel%
 
