553b0c76474e0291c30150473b215fdaa672f638
macintoshplus <jean-baptiste@nahan.fr>
2019-01-07 14:08:11 +0100

    set opt show slow only for 7.2 and 7.3

--- appveyor/test_task.bat
+++ appveyor/test_task.bat
@@ -32,8 +32,15 @@ mkdir c:\tests_tmp
 
 set TEST_PHP_JUNIT=c:\junit.out.xml
 
+set ADDOPT=" "
+if /i "%PHP_REL%" equ "7.2" (
+	set ADDOPT="  --show-slow 1000"
+)
+if /i "%PHP_REL%" equ "7.3" (
+	set ADDOPT="  --show-slow 1000"
+)
 rem cd "%APPVEYOR_BUILD_FOLDER%"
-nmake test TESTS="-q --offline --show-diff --show-slow 1000 --set-timeout 120 -g FAIL,XFAIL,BORK,WARN,LEAK,SKIP --temp-source c:\tests_tmp --temp-target c:\tests_tmp"
+nmake test TESTS="-q --offline --show-diff%ADDOPT% --set-timeout 120 -g FAIL,XFAIL,BORK,WARN,LEAK,SKIP --temp-source c:\tests_tmp --temp-target c:\tests_tmp"
 
 set EXIT_CODE=%errorlevel%
 
