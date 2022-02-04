37c01fea3869290ea6e816aaa7b0fdad5c5da8d3
Jean-Baptiste Nahan <814683+macintoshplus@users.noreply.github.com>
2022-02-04 20:40:36 +0100

    remove warning C4293

--- .github/build_task.cmd
+++ .github/build_task.cmd
@@ -31,7 +31,7 @@ setlocal enableextensions enabledelayedexpansion
 
 		if %errorlevel% neq 0 exit /b 3
 
-        set CFLAGS=/W1 /WX
+        set CFLAGS=/W1 /WX /WD4293
 
 		cmd /c configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --enable-object-out-dir=%PHP_BUILD_OBJ_DIR% --with-config-file-scan-dir=%GITHUB_WORKSPACE%\build\modules.d --with-prefix=%GITHUB_WORKSPACE%\build --with-php-build=%DEPS_DIR%
 
